d28343c2e9fb66e6b187240bc1711fe472bd5ab7
Joshua Thomas Giron <jthomasgiron@gmail.com>
2025-11-04 19:14:37 -0300

    se agregan animaciones al sprite de player

--- entities/player.py
+++ entities/player.py
@@ -1,11 +1,15 @@
 import pygame
+import os
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        # Sprite temporal (más adelante se reemplaza por animaciones)
-        self.image = pygame.Surface((40, 60))
-        self.image.fill((200, 80, 80))
+        self.animations = self.load_animations()
+        self.current_animation = 'idle'
+        self.frame_index = 0
+        self.aniamtion_speed = 0.15
+
+        self.image = self.animations[self.current_animation][self.frame_index]
         self.rect = self.image.get_rect(topleft=(x, y))
 
         # Físicas
@@ -16,9 +20,26 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.8
         self.on_ground = False
 
+    def load_animations(self):
+        # Cargar imagenes para las animaciones
+        animations =  {}
+        base_path = os.path.join("assets", "player")
+        for anim_name in os.listdir(base_path):
+            folder = os.path.join(base_path, anim_name)
+            if os.path.isdir(folder):
+                frames = []
+                for  filename in sorted(os.listdir(folder)):
+                    img_path = os.path.join(folder, filename)
+                    image = pygame.image.load(img_path).convert_alpha()
+                    frames.append(image)
+                animations[anim_name] = frames
+        return animations
+
+
     def update(self, keys):
         self.handle_input(keys)
         self.apply_gravity()
+        self.animate()  # Llamar a la animación para actualizar los frames
 
         # Movimiento horizontal
         self.rect.x += self.vel_x
@@ -28,13 +49,27 @@ class Player(pygame.sprite.Sprite):
         self.vel_x = 0
         if keys[pygame.K_a]:
             self.vel_x = -self.speed
-        if keys[pygame.K_d]:
+            self.current_animation = 'run'
+        elif keys[pygame.K_d]:
             self.vel_x = self.speed
+            self.current_animation = 'run'
+        else:
+            self.current_animation = 'idle'
+
+        
         if keys[pygame.K_SPACE] and self.on_ground:
             self.vel_y = self.jump_force
             self.on_ground = False
+            self.current_animation = 'jump'
 
     def apply_gravity(self):
         self.vel_y += self.gravity
         if self.vel_y > 15:  # límite de caída
             self.vel_y = 15
+
+    def animate(self):
+        frames = self.animations[self.current_animation]
+        self.frame_index += self.aniamtion_speed
+        if self.frame_index >= len(frames):
+            self.frame_index = 0
+        self.image = frames[int(self.frame_index)]
